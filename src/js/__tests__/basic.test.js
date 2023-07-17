a08dbed783075db38d14babb568fa90bf1f6e0d1
theo <edem.teo@gmail.com>
2023-07-17 13:35:52 +0300

    task 1

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,43 @@
-import sum from '../basic';
+import orderByProps from '../basic';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test('should show args first', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const firstProps = ['name', 'level'];
+  const orderedByProps = orderByProps(obj, firstProps);
 
-  expect(result).toBe(6);
+  expect(orderedByProps[0].key).toBe('name');
+  expect(orderedByProps[1].key).toBe('level');
+  expect(orderedByProps[2].key).toBe('health');
+  expect(orderedByProps[3].key).toBe('attack');
+  expect(orderedByProps[4].key).toBe('defence');
+});
+
+test('should throw for null object', () => {
+  const firstProps = ['name', 'level'];
+
+  expect(() => orderByProps(null, firstProps)).toThrow('null argument');
+});
+
+test('should ignore if null first props', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const orderedByProps = orderByProps(obj, null);
+
+  expect(orderedByProps[0].key).toBe('name');
+  expect(orderedByProps[1].key).toBe('health');
+  expect(orderedByProps[2].key).toBe('level');
+  expect(orderedByProps[3].key).toBe('attack');
+  expect(orderedByProps[4].key).toBe('defence');
+});
+
+test('should throw if not existing prop', () => {
+  const obj = {
+    name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+  };
+  const firstProps = ['SDFGHJKL', 'level'];
+
+  expect(() => orderByProps(obj, firstProps)).toThrow('not existing property');
 });
--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,34 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+export default function orderByProps(obj, firstProps) {
+  if (obj == null) {
+    throw new Error('null argument');
   }
-  return result;
+  if (firstProps != null
+    && !firstProps.every((prop) => Object.keys(obj).some((key) => prop === key))) {
+    throw new Error('not existing property');
+  }
+  const allPairs = [];
+  for (const prop in obj) {
+    if (Object.prototype.hasOwnProperty.call(obj, prop)) {
+      allPairs.push({
+        key: prop,
+        value: obj[prop],
+      });
+    }
+  }
+  if (firstProps == null) {
+    return allPairs;
+  }
+  const firstPairs = Array.from(
+    firstProps
+      .filter((prop) => firstProps.some((firstProp) => firstProp === prop))
+      .map((prop) => ({
+        key: prop,
+        value: obj[prop],
+      })),
+  );
+  const pairs = [];
+  firstPairs.forEach((pair) => pairs.push(pair));
+  allPairs.filter((pair) => !firstProps.some((prop) => pair.key === prop))
+    .forEach((pair) => pairs.push(pair));
+  return pairs;
 }
